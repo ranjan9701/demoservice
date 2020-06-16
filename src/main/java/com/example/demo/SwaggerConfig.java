6760eaa3f9b287b30177dbc0e9e6679c3364dd12
Soumyadipta Sarkar <soumyadipta@indusnet.co.in>
2020-06-16 21:17:18 +0530

    edfv

---
+++ src/main/java/com/example/demo/SwaggerConfig.java
@@ -0,0 +1,68 @@
+package com.example.demo;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@Import(BeanValidatorPluginsConfiguration.class)
+//public class SwaggerConfig extends WebMvcConfigurationSupport {
+public class SwaggerConfig {
+
+	@Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()    
+                .apis(RequestHandlerSelectors.basePackage("com.example.demo"))
+                .paths(regex("/*.*"))
+                .build()
+                .apiInfo(apiInfo())
+                .globalOperationParameters(
+                        Arrays.asList(new ParameterBuilder()
+                            .name("Authorization")
+                            .description("Description of header")
+                            .modelRef(new ModelRef("string"))
+                            .parameterType("header")
+                            .required(true)
+                            .build()));
+             
+    }
+	
+	private ApiInfo apiInfo() {
+	    Contact contact = new Contact("OPD Demo-Service", "", "");
+	    return new ApiInfoBuilder()
+	            .title("OPD Demo-Service REST API details")
+	            .description("OPD Demo-Service")
+	            .termsOfServiceUrl("https://github.com/sdcuike/spring-boot-practice")
+	            .contact(contact)
+	            .license("Apache License Version 2.0")
+	            .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+	            .version("2.0.0")
+	            .build();
+	}
+	
+//	@Override
+//    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("swagger-ui.html")
+//                .addResourceLocations("classpath:/META-INF/resources/");
+//
+//        registry.addResourceHandler("/webjars/**")
+//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+//    }
+}
